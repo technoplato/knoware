1e44e98f85a377649b24d1fc79f801584e777587
Michael Lustig <lustig@knophy.com>
2023-10-17 07:16:26 -0400

    Add machine and sky code

--- sky-machines/src/lib/sky-machines.spec.tsx
+++ sky-machines/src/lib/sky-machines.spec.tsx
@@ -1,10 +1,3 @@
-import { render } from '@testing-library/react';
-
-import SkyMachines from './sky-machines';
-
 describe('SkyMachines', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<SkyMachines />);
-    expect(baseElement).toBeTruthy();
-  });
+  // it('should render successfully', () => {});
 });
--- sky-machines/src/lib/sky-machines.tsx
+++ sky-machines/src/lib/sky-machines.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { useStatelyActor } from '@statelyai/sky-react';
+import { skyConfig } from './lights.sky';
+
+const skyurl = 'https://sky.stately.ai/5hIBJk';
+const sessionKey = 'shared';
 
 export const useFoo = () => {
-  const [state, setState] = React.useState(0);
+  const [snapshot, send, actor] = useStatelyActor(
+    {
+      apiKey: 'sta_e922f7a4-809c-4eb0-8311-0a1142dd3c57',
+      url: skyurl,
+      sessionId: sessionKey,
+    },
+    skyConfig
+  );
 
-  return [state, setState];
+  // const [state, setState] = React.useState(0);
+
+  // return [state, setState];
+
+  return [snapshot, send, actor];
 };
