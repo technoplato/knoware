9bb26ec46e5040fc920ee1188e0d742c0d498b50
Michael Lustig <lustig@knophy.com>
2023-10-19 09:57:04 -0400

    Delete other lib

--- other-lib/src/lib/other-lib.spec.ts
+++
@@ -1,7 +0,0 @@
-import { otherLib } from './other-lib';
-
-describe('otherLib', () => {
-  it('should work', () => {
-    expect(otherLib()).toEqual('I depend on machines and they are: machines');
-  });
-});
--- other-lib/src/lib/other-lib.ts
+++
@@ -1,5 +0,0 @@
-import { machines } from '@knoware/machines';
-export function otherLib(): string {
-  const whatAreMachines = machines();
-  return 'I depend on machines and they are: ' + whatAreMachines;
-}
