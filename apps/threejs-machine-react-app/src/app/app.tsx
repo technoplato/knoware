5a71ec131ce7c429bc035038c932b636a5ece96d
Michael Lustig <lustig@knophy.com>
2023-10-28 12:15:03 -0400

    Working threejs example

--- apps/threejs-machine-react-app/src/app/app.tsx
+++ apps/threejs-machine-react-app/src/app/app.tsx
@@ -1,7 +1,41 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef, useState } from 'react';
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta));
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+}
 export function App() {
-  return <div>Hello app</div>;
+  return (
+    <div>
+      Its running!
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+      , thats' not that
+    </div>
+  );
 }
 
 export default App;
