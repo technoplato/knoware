cdbb203728d889595b26fb2f64271774110d6916
Michael Lustig <lustig@knophy.com>
2024-03-12 11:32:20 -0400

    wallaby is working

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,11 @@ import { getJestProjects } from '@nx/jest';
 
 export default {
   projects: getJestProjects(),
+    preset: 'ts-jest',
+  testEnvironment: 'node',
+  // globals: {
+  //   'ts-jest': {
+  //     tsconfig: '<rootDir>/tsconfig.spec.json',
+  //   },
+  // },
 };
