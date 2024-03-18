e375f293a9674606374f56f0427cdc737c886d28
Michael Lustig <lustig@knophy.com>
2024-03-18 11:40:08 -0400

    feat: Add test for happy path scenario in Permission Monitoring Machine

---
+++ libs/permissions/permissionLogic/src/lib/permission-logic.spec.ts
@@ -0,0 +1,9 @@
+// permissionMonitoringMachine.test.ts
+
+describe('Permission Monitoring Machine', () => {
+  describe('Happy path', () => {
+    it('should work', () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
