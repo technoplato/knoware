0ebe3c50cc2daffb577e20110ff7f3e65618e568
Michael Lustig <lustig@knophy.com>
2024-04-22 15:44:44 -0400

    cleanup counting feature machine

--- libs/permissions/permissionLogic/src/lib/features/counting/counting.machine.ts
+++ libs/permissions/permissionLogic/src/lib/features/counting/counting.machine.ts
@@ -1,4 +1,4 @@
-import { assign, log, not, raise, sendTo, setup } from 'xstate';
+import { assign, not, sendTo, setup } from 'xstate';
 import {
   Permission,
   PermissionStatus,
@@ -15,8 +15,9 @@ export const countingMachineThatNeedsPermissionAt3 = setup({
   actions: {
     incrementCount: assign({ count: ({ context }) => context.count + 1 }),
     assignBluetoothStatusGranted: assign({ permissionStatus: 'granted' }),
-    triggerBluetoothPermissionRequest: raise({
-      type: 'permissionWasRequested',
+
+    triggerBluetoothPermissionRequest: sendTo('permissionReportingCounting', {
+      type: 'requestPermission',
       permission: Permissions.bluetooth,
     }),
   },
@@ -30,21 +31,18 @@ export const countingMachineThatNeedsPermissionAt3 = setup({
 
   types: {
     context: {} as { count: number; permissionStatus: PermissionStatus },
-    events:
-      {} as // TODO pull these out into their own discrete types and do a union here
+    events: {} as  // TODO pull these out into their own discrete types and do a union here
       | { type: 'count.inc' }
-        | { type: 'permissions.bluetooth.revoked' }
-        | { type: 'permissions.bluetooth.granted' }
-        | { type: 'permissions.bluetooth.denied' }
-        | { type: 'permissionWasRequested'; permission: Permission }
-        | {
-            type: 'user.didTapBluetoothRequestPermission';
-            permission: Permission;
-          },
+      | { type: 'permissions.bluetooth.revoked' }
+      | { type: 'permissions.bluetooth.granted' }
+      | { type: 'permissions.bluetooth.denied' }
+      | {
+          type: 'user.didTapBluetoothRequestPermission';
+          permission: Permission;
+        },
   },
 }).createMachine({
   /** @xstate-layout N4IgpgJg5mDOIC5QAoC2BDAxgCwJYDswBKAYgAcwAnVXWWXAe31gDoAjAGwFcwAXBhr2wtKYAG4MA1pADaABgC6iUGQb1ejfMpAAPRAFoAjADYArCwAshgJwAmABwBmC6YDst03YA0IAJ6JHQzlLazkrR1tHOUjHVwBfOJ80LDxCIhYwfHROSBJMBi58XhYCTHklJBBVdU1tPQR7IJZHY3tTW1tDQwtHa1NTH38EDosWe3s5Q1dJ02Mw21d4xJBknAJiDKyciBJy7WrcDSY6xGtrexZDRrlo+wtbMPvBxAXDFmiruVMJszlxhKSGDWaU22Q4uRkhgqKjUh1qlXqZwuVyCt3uj1szwQzmCrnsxiseIJ1kcpgBKyBqQ22HQ+AgHAIUAAClQaHRNLByKzaPQmCwoJRabxIOxuHwBEI9pUDkctAjEEFDOZ+u4LCS7v0OliOm8bMYHN97rYLHJnOTVlT0jS6Qz8MzueymJyKNQeZoWBBMrgRZwePxBNgpTCasd5QhumdmmZQk5rMZrPcLNr+s17LZ44Z0+FFubKet0r7xQGACJe3Iutm8-D8wVFH1i-2SxT7WGyk7htXWKOeP69eOJrGolh40kefGTOTGbq5lL50V+iXYUv4b07LiwKge3AQAAq6DIACEG4uAEpgACOPFgvBZrsd+CDVVb8NA9Sm7jGcjOU46E08Sb8BU3GHVxPBsNNHFiCwLCWQFZxBQtG2wM8JGkHYKzdPkBSFesFwDR8ZRfXREFMFoWDsaD4wcCI8VcLFAlcYdHHxRZwlMcCLBnYENkQ09xCkXJ103CBtz3Q9jwDM9LzgG8HSrAjn1DV8FScFhbHOQxmMmB4E2sbUphYYxAlCTx3FA4xjC4y15yLIQUIEtcN0oLdd33AA5dAxFwKB0GFHcGAAZT4DQ7VgBSQzlZThgM9w7msTTmNCLpjEHDpmjcExpjOVwpgJBJlnwBhPXgSoLXzFsIvbfROkcSwbGolx3E8TFAIQfR2ijKdzkCdSVVcTjljKkFMjBSAKrhJTiIaZj3jjWJnEg1pWnojxLlMNUv2iGMpisudrXpRlb0rDlxrbMNMwTd5JiiHVZnjbUIkuUlQm6rTJ1gil4J4iShGXVdTqIt98TeexznY6YLHGfU6Nasd3lMSdvnjAlNJcXaEJ+5D+LQgHJvqdNjDGIJ2i-eMvlCAChgY8jJwJSHGr6SzBrzEEADMCFobAxulRTIqmi7RhuTTok6O69NayDGJcTt02+frAjJfKgA */
-  // type: 'parallel',
   context: {
     count: 0,
     permissionStatus: PermissionStatuses.unasked,
@@ -72,12 +70,12 @@ export const countingMachineThatNeedsPermissionAt3 = setup({
     enabled: {
       always: [
         {
-          target: 'handlingPermissions',
           guard: 'isPermissionRequiredToContinue',
+          target: 'handlingPermissions',
         },
         {
-          target: 'finished',
           guard: 'isCountingCompleted',
+          target: 'finished',
         },
       ],
       on: {
@@ -90,12 +88,7 @@ export const countingMachineThatNeedsPermissionAt3 = setup({
     handlingPermissions: {
       on: {
         'user.didTapBluetoothRequestPermission': {
-          actions: sendTo('permissionReportingCounting', ({ event }) => {
-            return {
-              type: 'requestPermission',
-              permission: Permissions.bluetooth,
-            };
-          }),
+          actions: 'triggerBluetoothPermissionRequest',
         },
 
         'permissions.bluetooth.granted': {
@@ -110,12 +103,7 @@ export const countingMachineThatNeedsPermissionAt3 = setup({
       on: {
         'permissions.bluetooth.granted': {
           target: 'enabled',
-          actions: [
-            'assignBluetoothStatusGranted',
-            log(({ event }) => {
-              console.log(JSON.stringify(event, null, 2));
-            }),
-          ],
+          actions: ['assignBluetoothStatusGranted'],
         },
         'user.didTapBluetoothRequestPermission': {
           actions: 'triggerBluetoothPermissionRequest',
