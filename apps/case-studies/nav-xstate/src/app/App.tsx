4ea827349b3e9efaceab1d9b0d6c64c097dfd628
Michael Lustig <lustig@knophy.com>
2024-04-29 21:52:45 -0400

    fuck nx
     ERROR  TypeError: Cannot read property 'useRef' of null
    
    This error is located at:
        in NavigationContainerInner (created by App)
        in App
        in RCTView (created by View)
        in View (created by AppContainer)
        in RCTView (created by View)
        in View (created by AppContainer)
        in AppContainer
        in NavXstateCaseStudy(RootComponent), js engine: hermes
     ERROR  TypeError: Cannot read property 'useRef' of null

--- apps/case-studies/nav-xstate/src/app/App.tsx
+++ apps/case-studies/nav-xstate/src/app/App.tsx
@@ -1,10 +1,41 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { Text, View } from 'react-native';
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: { userId: number };
+};
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const App = () => {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <HomeScreen />
+      {/* // <Stack.Navigator>
+    //   <Stack.Screen name="Home" component={HomeScreen} />
+    //   <Stack.Screen name="Details" component={DetailsScreen} />
+    // </Stack.Navigator> */}
+    </NavigationContainer>
   );
 };
 
